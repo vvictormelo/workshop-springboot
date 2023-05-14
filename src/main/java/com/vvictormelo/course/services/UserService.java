4a711331776abdcf429e34bca133eb7d5ad24fad
Victor Melo <melovictor515@gmail.com>
2023-05-14 20:00:18 -0300

    Service layer, component registration

---
+++ src/main/java/com/vvictormelo/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.vvictormelo.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vvictormelo.course.entities.User;
+import com.vvictormelo.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long Id) {
+		Optional<User> obj = repository.findById(Id);
+		return obj.get();
+	}
+}
